447e0fea72d9bf598328c51fa2072945b750e6b2
Arthur Damm <indigoarthur@gmail.com>
2019-06-10 05:16:41 +0000

    Add exceptions, documentation, clean up.

--- level_0/vote.py
+++ level_0/vote.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
-'''Module for Hodor level 0 challenge.'''
+'''
+Module for Hodor level 0 challenge.
+
+Has to add the 'id' and 'holdthedoor' fields to a proper POST request.
+
+'''
 import requests
 
 url = 'http://158.69.76.135/level0.php'
 data = {'id': '650', 'holdthedoor': 'Submit'}
+success = 'Hold the Door challenge - Level 0'
 votes = 1024
 ok = 0
+fail = 0
 
-for i in range(votes):
-    response = requests.post(url, data=data)
-    if response.status_code is 200:
-        ok += 1
+print("Starting {} votes:".format(votes))
 
-print("Done! {}/{} votes submitted.".format(ok, votes))
+while ok < votes and fail < 100:
+    try:
+        response = requests.post(url, data=data)
+        if response.status_code is 200 and success in response.text:
+            fail = 0
+            ok += 1
+            print("{} ok!".format(ok))
+    except Exception as e:
+        print(e)
+        fail += 1
+
+print("Finished: {}/{} votes.".format(ok, votes))
